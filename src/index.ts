a5a8a46db3613a4a6c1b304e5528cea727f942cd
Nick Barnette <nicdavid@live.unc.edu>
2021-01-09 14:28:23 -0500

    adding navigation bar

--- src/index.ts
+++ src/index.ts
@@ -60,6 +60,9 @@ const checkIfInView = (el: HTMLElement, offset?: number) => {
 	return inView;
 };
 
+/**
+ * Check if it is time to animate content
+ */
 window.addEventListener('scroll', () => {
 	const animatedElements = Array.from(
 		document.getElementsByClassName('will-animate-rise'),
@@ -89,5 +92,89 @@ window.addEventListener('load', () => {
 			el.classList.add('animated-rise');
 			el.classList.remove('will-animate-rise-onload');
 		}
+	}
+});
+
+/**
+ * Check if the navigation bar needs to change styling
+ */
+let navigationScrolling = false;
+const sections = ['mission', 'program', 'attendees', 'team', 'contact'];
+const handleNavigationUpdates = () => {
+	const navigation = document.getElementById('navigation');
+	const hero = document.getElementById('particles-js');
+	if (!navigation || !hero) return;
+
+	// Check inversion
+	if (
+		window.scrollY + navigation?.getBoundingClientRect().height >=
+		hero?.getBoundingClientRect().height
+	) {
+		navigation.classList.add('navigation-inverted');
+	} else {
+		navigation.classList.remove('navigation-inverted');
+	}
+
+	if (navigationScrolling) return;
+
+	// Check location
+	let foundElement = false;
+	for (let l of sections) {
+		const el = document.getElementById(l);
+		if (!el) continue;
+		const lnk = document.getElementById(`${l}-link`);
+		if (!lnk) continue;
+		if (checkIfInView(el, 250) && !foundElement) {
+			lnk.classList.add('navigation-link-active');
+			foundElement = true;
+		} else {
+			lnk.classList.remove('navigation-link-active');
+		}
+	}
+};
+window.addEventListener('scroll', handleNavigationUpdates);
+window.addEventListener('load', handleNavigationUpdates);
+window.addEventListener('load', () => {
+	const lnks = Array.from(
+		document.getElementsByClassName('navigation-link'),
+	) as HTMLElement[];
+	for (let a of lnks) {
+		a.addEventListener('click', () => {
+			navigationScrolling = true;
+			const l = a.getAttribute('data-section');
+			if (!l) return;
+			const el = document.getElementById(l);
+			if (!el) return;
+
+			// Start scroll
+			window.scrollTo({
+				top: window.scrollY + el?.getBoundingClientRect().y,
+				left: 0,
+				behavior: 'smooth',
+			});
+
+			// Reset scrolling
+			const interval = setInterval(() => {
+				if (
+					el?.getBoundingClientRect().y === 0 ||
+					document.body.scrollHeight - window.innerHeight ===
+						window.scrollY
+				) {
+					navigationScrolling = false;
+					clearInterval(interval);
+				}
+			}, 100);
+
+			// Update the active link
+			for (let s of sections) {
+				const lnk = document.getElementById(`${s}-link`);
+				if (!lnk) continue;
+				if (s === l) {
+					lnk.classList.add('navigation-link-active');
+				} else {
+					lnk.classList.remove('navigation-link-active');
+				}
+			}
+		});
 	}
 });
